dbe3d8e8b46588c10ef60ef542e5fefbc552d8ff
tarchan <tarchan@gmail.com>
2011-06-16 12:01:03 +0900

    ステータスバーを生成する機能を追加

---
+++ src/com/mac/tarchan/desktop/StatusBar.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2011 tarchan. All rights reserved.
+ */
+package com.mac.tarchan.desktop;
+
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JSeparator;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * ステータスバーを生成します。
+ */
+@SuppressWarnings("serial")
+public class StatusBar extends Box
+{
+	/**
+	 * 空のステータスバーを構築します。
+	 */
+	public StatusBar()
+	{
+		super(BoxLayout.LINE_AXIS);
+		setBorder(new EmptyBorder(2, 4, 2, 4));
+	}
+
+	/**
+	 * セパレータを追加します。
+	 * 
+	 * @return StatusBar
+	 */
+	public StatusBar addSeparator()
+	{
+		JSeparator sep = new JSeparator(JSeparator.VERTICAL);
+		sep.setMaximumSize(new Dimension(2, 16));
+		sep.setMinimumSize(new Dimension(2, 16));
+//		sep.setBorder(new EmptyBorder(8, 8, 8, 8));
+
+		add(Box.createRigidArea(new Dimension(4, 4)));
+		add(sep);
+		add(Box.createRigidArea(new Dimension(4, 4)));
+
+		return this;
+	}
+}
