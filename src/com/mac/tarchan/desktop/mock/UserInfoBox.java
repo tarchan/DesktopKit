1e4671c8a916473694af2c322190002a50941a22
tarchan <tarchan@gmail.com>
2011-07-10 22:31:27 +0900

    テーブルサンプルにアクションボタンを追加

--- src/com/mac/tarchan/desktop/mock/UserInfoBox.java
+++ src/com/mac/tarchan/desktop/mock/UserInfoBox.java
@@ -1,12 +1,16 @@
 package com.mac.tarchan.desktop.mock;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Window;
 
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import com.mac.tarchan.desktop.DesktopSupport;
@@ -37,14 +41,26 @@ public class UserInfoBox
 
 	Component createMain()
 	{
-		String[] header = {"ニックネーム", "ユーザー名", "アドレス"};
+		String[] header = {"ニックネーム", "ログイン名", "アドレス", "名前", "サーバ"};
 		DefaultTableModel model = new DefaultTableModel(header, 10);
 		JTable table = new JTable(model);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);
 		JScrollPane sp = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
-		JPanel main = new JPanel();
-		main.add(sp);
+		JButton update = new JButton("更新");
+		JButton save = new JButton("ファイルに保存");
+		JButton close = new JButton("閉じる");
+
+		Box box = Box.createHorizontalBox();
+		box.setBorder(new EmptyBorder(4, 12, 4, 12));
+		box.add(Box.createHorizontalGlue());
+		box.add(update);
+		box.add(save);
+		box.add(close);
+
+		JPanel main = new JPanel(new BorderLayout());
+		main.add(sp, BorderLayout.CENTER);
+		main.add(box, BorderLayout.SOUTH);
 		return main;
 	}
 }
